c8cb9748894cbe4ef0336fe5bccc55e53401e61f
Marios Andreopoulos <opensource@andmarios.com>
2015-06-24 17:16:24 +0300

    minor: disable some custom appearance settings for emacs, support asciidoc extensions (.adoc and .asciidoc)

--- devenv/home/.emacs.d/init.el
+++ devenv/home/.emacs.d/init.el
@@ -59,25 +59,25 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
- '(column-marker-1 ((t (:background "red"))))
- '(diff-added ((t (:foreground "cyan"))))
- '(flymake-errline ((((class color) (background light)) (:background "Red"))))
- '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
- '(fundamental-mode-default ((t (:inherit default))))
- '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
- '(linum ((t (:foreground "black" :weight bold))))
- '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
- '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
- '(show-paren-match ((((class color) (background light)) (:background "black"))))
- '(vertical-border ((t nil)))
-)
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+;;  '(column-marker-1 ((t (:background "red"))))
+;;  '(diff-added ((t (:foreground "cyan"))))
+;;  '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+;;  '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+;;  '(fundamental-mode-default ((t (:inherit default))))
+;;  '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+;;  '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+;;  '(linum ((t (:foreground "black" :weight bold))))
+;;  '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+;;  '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+;;  '(show-paren-match ((((class color) (background light)) (:background "black"))))
+;;  '(vertical-border ((t nil)))
+;; )
 
 ;; ------------
 ;; -- Macros --
@@ -176,3 +176,6 @@
 
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.adoc$" . adoc-mode))
+(add-to-list 'auto-mode-alist '("\\.asciidoc$" . adoc-mode))
